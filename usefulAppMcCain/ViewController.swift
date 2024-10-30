b89b1c7b912749317cbc67360b4bb80bcc6dc461
seanmccain08 <seanmccain327@gmail.com>
2024-10-30 07:37:25 -0500

    Initial Commit

---
+++ usefulAppMcCain/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  usefulAppMcCain
+//
+//  Created by SEAN MCCAIN on 10/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
