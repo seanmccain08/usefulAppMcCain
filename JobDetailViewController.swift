c40115c6041f957a566161d8f1928b48189dde62
seanmccain08 <seanmccain327@gmail.com>
2024-11-06 08:10:49 -0600

    made some changes

---
+++ JobDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  JobDetailViewController.swift
+//  usefulAppMcCain
+//
+//  Created by SEAN MCCAIN on 11/6/24.
+//
+
+import UIKit
+
+class JobDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
