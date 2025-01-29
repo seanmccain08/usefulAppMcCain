f7c9d1fab21ce61a7ec9ed07350344aeb72e188d
seanmccain08 <seanmccain327@gmail.com>
2025-01-29 08:06:26 -0600

    added some stuff

---
+++ usefulAppMcCain/MapViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MapViewController.swift
+//  usefulAppMcCain
+//
+//  Created by SEAN MCCAIN on 1/28/25.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
